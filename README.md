e84d5ac79eca1fbce3a648dc83f1db035558a7b5
rod <rod@rods-MacBook-Pro.local>
2012-04-21 17:15:07 +0200

    update 1 - v0.9.5

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ many-tips-together
 ==================
 
 Plugin test bed, this is the official one: http://wordpress.org/extend/plugins/many-tips-together
+
+~Current Version:0.9.5~
--- many-tips-together.php
+++ many-tips-together.php
@@ -20,7 +20,21 @@ License: GPL
 define('MTT_LOGO_HEIGHT', 300);
 define('MTT_LOGIN_BACKGROUND', 'repeat');
 define('MTT_VERSION', '0.9.4');
-
+include_once('updater.php');
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+	    'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+	    'proper_folder_name' => 'many-tips-together', // this is the name of the folder your plugin lives in
+	    'api_url' => 'https://api.github.com/repos/brasofilo/many-tips-together', // the github API url of your github repo
+	    'raw_url' => 'https://raw.github.com/brasofilo/master/many-tips-together', // the github raw url of your github repo
+	    'github_url' => 'https://github.com/brasofilo/many-tips-together', // the github url of your github repo
+	    'zip_url' => 'https://github.com/brasofilo/many-tips-together/zipball/master', // the zip url of the github repo
+        'requires' => '3.0', // which version of WordPress does your plugin require?
+        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+    );
+    new wp_github_updater($config);
+}
 if (!class_exists("ManyTips")) {
 	class ManyTips {
 		// REFERENCE VALUE TO DATABASE
